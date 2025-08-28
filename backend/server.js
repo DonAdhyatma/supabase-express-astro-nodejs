a5e91508e498c9449c8d152d86c5250ebc912ca1
Danni Adhyatma <dannirachman2@gmail.com>
2025-08-28 13:42:20 +0700

    feat: confirm update feature from dependabot

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
-const express = require("express")
-const supabase = require("@supabase/supabase-js")
+const express = require("express");
+const supabase = require("@supabase/supabase-js");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const PORT = 3211 || process.env.PORT
+const PORT = 3211 || process.env.PORT;
 
-const SUPABASE_URL = "https://staebusbmbzoifcyrudr.supabase.co"
-const SUPABASE_SERVICE_ROLE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0YWVidXNibWJ6b2lmY3lydWRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjM4NDY5MiwiZXhwIjoyMDI3OTYwNjkyfQ.0YP72IbBY4XuWfs7FcK8gew-lEahqcTUgMtbULwh5aQ"
+const SUPABASE_URL = "https://pstsddgnmsxonjyziofw.supabase.co";
+const SUPABASE_SERVICE_ROLE =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzdHNkZGdubXN4b25qeXppb2Z3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYzNTk0NjgsImV4cCI6MjA3MTkzNTQ2OH0.MCri78AyKTwWbLhh1izAGq59JMWiHbZn3jdYP4UP-rw";
 
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE)
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
 
 app.get("/", async (request, response) => {
-    const getBlog = await db.from("blog").select()
-    response.json({ getBlog })
-})
+  const getBlog = await db.from("blog").select();
+  response.json({ getBlog });
+});
 
 app.post("/", async (request, response) => {
-    const { title, description } = request.body
-    const createPost = await db.from("blog").insert({ title, description })
-    console.log("🚀 ~ app.post ~ createPost:", createPost)
+  const { title, description } = request.body;
+  const createPost = await db.from("blog").insert({ title, description });
+  console.log("🚀 ~ app.post ~ createPost:", createPost);
 
-    response.json({ createPost })
-})
+  response.json({ createPost });
+});
 
 app.listen(PORT, () => {
-    console.log("server running on port ", PORT)
-})
+  console.log("server running on port ", PORT);
+});
